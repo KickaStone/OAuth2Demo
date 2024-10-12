a0407cf38b3d3ce1fe6991762a89a9a9c9edd808
jijuncheng <jijuncheng@sjtu.edu.cn>
2024-10-13 04:06:27 +0800

    Add get access_token and refresh token

--- src/main/java/org/example/oauth2demo/OAuthController.java
+++ src/main/java/org/example/oauth2demo/OAuthController.java
@@ -4,16 +4,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import java.util.Map;
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class OAuthController {
 
-    boolean isAuthenticated = false;
+    boolean hasCode = false;
+    boolean hasToken = false;
     String accessToken = null;
+    String refreshToken = null;
+    String code = null;
+    @RequestMapping("/oauth/callback")
+    public String callback(HttpServletRequest request) {
+        // get accessToken
+        code = request.getParameter("code");
+        if (code != null) {
+            hasCode = true;
+        } 
+        return "redirect:/oauth";
+    }
 
     @RequestMapping("/oauth")
     public ModelAndView oauth(Map<String, Object> model) {
-        model.put("isAuthenticated", isAuthenticated);
+        model.put("hasCode", hasCode);
+        model.put("hasToken", hasToken);
         model.put("accessToken", accessToken);
+        model.put("refreshToken", refreshToken);
+        model.put("code", code);
         return new ModelAndView("oauth");
     }
 }
--- src/main/resources/static/oauth.js
+++ src/main/resources/static/oauth.js
@@ -1,30 +1,37 @@
 button = document.getElementById("OAuth_Button")
 
-redirect_uri = "https://localhost:8080/oauth"
-client_id = "604421121209-40j3h23i02eulnvn12lgakd2jtdb0t9p.apps.googleusercontent.com"
-scope = "https://www.googleapis.com/auth/drive"
-access_type = "offline"
-response_type = "token"
+let oauth2CodeEndpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
+let oauth2TokenEndpoint = 'https://oauth2.googleapis.com/token';
+let redirect_uri = "https://localhost:8443/oauth/callback";
+let scope = "https://www.googleapis.com/auth/drive";
+let access_type = "offline";
+let response_type = "code";
+
+
+let client_id = "your-client-id";
+let client_secret = "your-client-secret";
 
 /*
  * Create form to request access token from Google's OAuth 2.0 server.
  */
 function oauthSignIn() {
-    // Google's OAuth 2.0 endpoint for requesting an access token
-    var oauth2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
-
     // Create <form> element to submit parameters to OAuth 2.0 endpoint.
     var form = document.createElement('form');
+    form.setAttribute('id', 'oauth2-form-get-code');
     form.setAttribute('method', 'GET'); // Send as a GET request.
-    form.setAttribute('action', oauth2Endpoint);
+    form.setAttribute('action', oauth2CodeEndpoint);
 
     // Parameters to pass to OAuth 2.0 endpoint.
-    var params = {'client_id': client_id,
+    var params = {
+        'client_id': client_id,
         'redirect_uri': redirect_uri,
         'response_type': response_type,
+        'access_type': access_type,
         'scope': scope,
         'include_granted_scopes': 'true',
-        'state': 'pass-through value'};
+        'state': 'pass-through value',
+        'prompt': 'consent'
+    };
 
     // Add form parameters as hidden input values.
     for (var p in params) {
@@ -40,28 +47,106 @@ function oauthSignIn() {
     form.submit();
 }
 
-function handleOAuthCallback() {
-    // if no parameters, check if we are already authenticated
-    if(window.location.hash.length === 0) {
-        if(isAuthenticated) {
-            console.log("Already authenticated");
-            return;
-        }else{
-            console.log("Not authenticated, signing in");
-            oauthSignIn();
-        }
-    }else{
-        // if there are parameters, parse the access token
-        const urlParams = new URLSearchParams(window.location.hash.substring(1));
-        const accessToken = urlParams.get('access_token');
 
-        if(accessToken) {
-            console.log("Access token: ", accessToken);
-            isAuthenticated = true;
-        } else {
-            console.log("No access token found");
-            isAuthenticated = false;
-        }
+function getAccessToken(refresh = false) {
+    form = document.createElement('form');
+    form.setAttribute('id', 'oauth2-form-get-token');
+    form.setAttribute('method', 'POST');
+    form.setAttribute('action', oauth2TokenEndpoint);
+    form.setAttribute('content-type', 'application/x-www-form-urlencoded');
+
+    var params = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': refreshToken ? 'refresh_token' : 'authorization_code',
+    };
+
+    if (refresh) {
+        params['refresh_token'] = refreshToken;
+    } else {
+        params['redirect_uri'] = redirect_uri;
+        params['code'] = code;
+    }
+    console.log(params);
+
+    fetch(oauth2TokenEndpoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: Object.keys(params).map(key => `${key}=${params[key]}`).join('&')
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            accessToken = data.access_token;
+            if (!refresh) {
+                refreshToken = data.refresh_token;
+            }
+            hasToken = accessToken != null && refreshToken != null;
+            handleOAuthCallback();
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function refreshAccessToken() {
+    getAccessToken(true);
+}
+
+function revokeAccess(accessToken) {
+    // Google's OAuth 2.0 endpoint for revoking access tokens.
+    var revokeTokenEndpoint = 'https://oauth2.googleapis.com/revoke';
+
+    // Create <form> element to use to POST data to the OAuth 2.0 endpoint.
+    var form = document.createElement('form');
+    form.setAttribute('method', 'post');
+    form.setAttribute('action', revokeTokenEndpoint);
+
+    // Add access token to the form so it is set as value of 'token' parameter.
+    // This corresponds to the sample curl request, where the URL is:
+    //      https://oauth2.googleapis.com/revoke?token={token}
+    var tokenField = document.createElement('input');
+    tokenField.setAttribute('type', 'hidden');
+    tokenField.setAttribute('name', 'token');
+    tokenField.setAttribute('value', accessToken);
+    form.appendChild(tokenField);
+
+    // Add form to page and submit it to actually revoke the token.
+    document.body.appendChild(form);
+    form.submit();
+}
+
+function handleOAuthCallback() {
+    let pCode = document.getElementById("oauth-code-text");
+    let pAccessToken = document.getElementById("oauth-access-token-text");
+    let pRefreshToken = document.getElementById("oauth-refresh-token-text");
+    let buttonLogout = document.getElementById("oauth-logout-button");
+    let buttonRefresh = document.getElementById("oauth-refresh-token-button");
+    pCode.textContent = "code: " + code;
+    pAccessToken.textContent = "access token: " + accessToken;
+    pRefreshToken.textContent = "refresh token: " + refreshToken;
+
+    if (hasCode && hasToken) {
+        console.log("User has authenticated.");
+        buttonRefresh.disabled = false;
+        buttonRefresh.addEventListener("click", () => refreshAccessToken());
+    } else {
+        let buttonCode = document.getElementById("oauth-code-button");
+        let buttonToken = document.getElementById("oauth-token-button");
+        buttonCode.disabled = hasCode;
+        buttonToken.disabled = hasToken;
+        buttonCode.addEventListener("click", () => oauthSignIn());
+        buttonToken.addEventListener("click", () => getAccessToken(false));
+        buttonRefresh.addEventListener("click", () => refreshAccessToken(true));
+        buttonLogout.addEventListener("click", () => {
+            code = null;
+            accessToken = null;
+            refreshToken = null;
+            hasCode = false;
+            hasToken = false;
+            if (accessToken) revokeAccess(accessToken);
+            handleOAuthCallback();
+        });
     }
 }
 
